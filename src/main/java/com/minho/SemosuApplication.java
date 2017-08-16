c5a6cbe9ba64e1ffb72d631f06320d0f4781ecd2
eastflag <eastflag@gmail.com>
2017-08-17 08:48:20 +0900

    first commit

---
+++ src/main/java/com/minho/SemosuApplication.java
@@ -0,0 +1,12 @@
+package com.minho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemosuApplication.class, args);
+	}
+}
